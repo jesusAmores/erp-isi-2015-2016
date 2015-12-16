79f0d38d17886580c81906d53c63e40fcd0d8735
root <root@ubuntu-VirtualBox>
2015-12-16 17:28:51 +0100

    pull request

--- src/GestionClientes/borrar.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $handle = new SQLite3('cliente.db') or die("cannot open the database");
-    
-    $handle->exec("DELETE FROM clientes WHERE id=".$_GET['id']);
-    header("Location:index.php");
-
-?>
